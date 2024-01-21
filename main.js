975099dbb63d0f53b5501565635c84baf6961a82
Glitch (internal-lackadaisical-carp) <none>
2024-01-21 07:02:33 +0000

    👵🏦 Checkpoint ./main.js:64364301/8062

--- main.js
+++ main.js
@@ -1,143 +1,103 @@
+/*
+ * Starter Project for WhatsApp Echo Bot Tutorial
+ *
+ * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
+ *
+ */
 
 "use strict";
 
-const express = require("express")
-let axios = require("axios")
-const event = require('./src/events/event') 
-const auth  = require('./auth/auth')
-const { google } = require('googleapis');
-const { OAuth2Client } = require('google-auth-library');
-require('dotenv').config()
+// Access token for your app
+// (copy token from DevX getting started page
+// and save it as environment variable into the .env file)
+const token = process.env.WHATSAPP_TOKEN;
 
-const app = express()
+// Imports dependencies and set up http server
+ const express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default,
+  app = express().use(body_parser.json()); // creates express http server
 
-app.use(express.json())
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-const now = new Date();
-const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000); // One hour later
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", (req, res) => {
+  // Parse the request body from the POST
+  let body = req.body;
 
-// const GOOGLE_CALENDAR_API_KEY = 'diwadev1214@gmail.com';
+  // Check the Incoming webhook message
+  console.log(JSON.stringify(req.body, null, 2));
 
+  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      
+      if(body == "Hai"){
+          console.log('')
+              axios({
+                method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+                url:
+                  "https://graph.facebook.com/v12.0/" +
+                  phone_number_id +
+                  "/messages?access_token=" +
+                  token,
+                data: {
+                  messaging_product: "whatsapp",
+                  to: from,
+                  text: { body: "Ack: " + msg_body },
+                },
+                headers: { "Content-Type": "application/json" },
+              });
+                 res.sendStatus(200);
 
-app.get('/webhook',function(req,res){
-  console.log(req,"req");  
-  const mode = req.query["hub.mode"]
-  const challenges = req.query['hub.challenge']
-  const verify_token = req.query['hub.verify_token']
-
-  const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN 
-
-  if(mode && challenges){
-      if(WHATSAPP_TOKEN == verify_token){
-           res.status(200).send(challenges)
       }
-  }
-  else{
-     res.status(403)
-  }
-})
-
-app.post('/webhook', async function(req,res){
-    const body_params = req.body
-    console.log(JSON.stringify(body_params.entry),"body_params");
-
-    if(body_params.object){
-
-
-        if(body_params.entry && body_params.entry[0].changes && body_params.entry[0].changes[0].value.messages && body_params.entry[0].changes[0].value.messages[0]){
-            let phone_no_id =  body_params.entry[0].changes[0].value.metadata.phone_number_id
-            let from  = body_params.entry[0].changes[0].value.messages[0].from
-            let body = ''
-            console.log(body_params.entry[0].changes[0].value.messages[0].type,"type");
-            if(body_params.entry[0].changes[0].value.messages[0].type == "text"){
-                 body = body_params.entry[0].changes[0].value.messages[0].text.body
-            }
-            else if(body_params.entry[0].changes[0].value.messages[0].type == "button"){
-                body = body_params.entry[0].changes[0].value.messages[0].button.text
-            }
-
-            
-            let url = `https://graph.facebook.com/v17.0/${phone_no_id}/messages?access_token=${process.env.TOKEN}`
-
-            let option ={
-                url,
-                phone_no_id,
-                from,
-                body
-            }
-
-           if(body == "Hai"){
-                await axios.post(url,{
-                    "messaging_product": "whatsapp",
-                    "to":from,
-                    "type":"template",
-                    "template":{
-                        "name":"welcome_message",
-                        "language":{
-                            "code":"en"
-                        }
-                    }
-                })
-                res.status(200).send("success")
-           }
-
-           else if(body == "Can you book me a time today that is available?"){
-                await axios.post(url,{
-                    "messaging_product": "whatsapp",
-                    "to":from,
-                    "type":"template",
-                    "template":{
-                        "name":"confirm_text",
-                        "language":{
-                            "code":"en"
-                        }
-                    }
-                })
-                res.status(200).send("success")
-           }
-           else if(body == "Yes" || body == "yes"){
-                let eventCreated = await auth.authorize().then(event.createEvent).catch(console.error);
-                if(eventCreated){
-                    await axios.post(url,{
-                        "messaging_product": "whatsapp",
-                        "to":from,
-                        "type":"text",
-                        "text":{
-                            "preview_url":true,
-                             "body":`Your Appointment is confirmed id ${eventCreated.id} `
-                        }
-                    })
-                }
-           }
-
-           else{
-              res.status(403).send("failure")
-           }
-
-        }
     }
 
-})
+  } else {
+    // Return a '404 Not Found' if event is not from a WhatsApp API
+    res.sendStatus(404);
+  }
+});
 
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
+app.get("/webhook", (req, res) => {
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+  **/
+  const verify_token = process.env.VERIFY_TOKEN;
 
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
 
 app.get("/",function(req,res){
     res.send("Whatsapp server")
-})
-
-app.post('/create_event', async function(req,res){
-
-    try {
-        let eventCreated = await auth.authorize().then(event.createEvent).catch(console.error);
-        return res.send(eventCreated)
-    } catch (error) {
-        console.error('Error creating Google Calendar event:', error);
-        res.status(500).send("Internal Server Error");
-    }
-})
-
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(` Server Listening ${process.env.PORT}`)
 })
